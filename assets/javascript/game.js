f07125c85c6f687254db55b474d1ab15945b86af
Michelangelo <youngbrennin20@gmail.com>
2018-05-30 23:29:59 -0700

    all that is left is theme song and reset

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,26 +1,59 @@
 var computerNumber = Math.floor(Math.random() * 51) + 50;
 var wins = 0;
 var losses= 0;
-var mushroomVal = Math.floor(Math.random() * 20) + 1;
-var flowerVal = Math.floor(Math.random() * 20) + 1;
-var featherVal = Math.floor(Math.random() * 20) + 1;
-var starVal = Math.floor(Math.random() * 20) + 1;
+var mushroomVal = Math.floor(Math.random() * 11) + 1;
+var flowerVal = Math.floor(Math.random() * 11) + 1;
+var featherVal = Math.floor(Math.random() * 11) + 1;
+var starVal = Math.floor(Math.random() * 11) + 1;
 var totalScore = 0;
 
-$(document).ready(function(){
-    
-    $(".button").click(function(event){
-        var choice = event.target.id
 
+
+
+$(document).ready(function(){
+
+    $(".button").click(function(event){
+        var choice = event.target.id;
+
+ 
         if (choice === "mushroom"){
-            totalScore += mushroomVal
+            totalScore += mushroomVal;
         }
 
+        else if (choice === "flower") {
+            totalScore += flowerVal;
+        }
 
+        else if (choice === "feather") {
+            totalScore += featherVal;
+        }
+
+        else if (choice === "star") {
+            totalScore += starVal;
+        }
+
+        if (totalScore === computerNumber) {
+            wins++;
+            $(".wins").text(wins);
+                var winner = document.getElementById("winner");
+                winner.play();
+
+
+        } else if (totalScore > computerNumber) {
+            losses++;
+            $(".losses").text(losses);
+                var loser = document.getElementById("loser");
+                loser.play();
+        }
 
         $(".total-score").text(totalScore);
         
     });
+
+
+
+
+
 $(".comp-numb").text(computerNumber);
 
 //finish if statement with else if's
@@ -28,6 +61,9 @@ $(".comp-numb").text(computerNumber);
 //wins++ or loss++ if over
 //reset values after win or loss
 
+});
 
-})
-
+function play(){
+    var audio = document.getElementById("audio");
+    audio.play();
+}
